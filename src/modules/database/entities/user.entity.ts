de79d1b5dfa4ce0b047f8fec5fa94cfda3066ddb
Can Dat <datcv@deepcare.io>
2025-03-31 10:16:50 +0700

    fix: First commit

---
+++ src/modules/database/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { UserRole } from '../enums/user-role.enum';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.RECEPTIONIST,
+  })
+  role: UserRole;
+}
